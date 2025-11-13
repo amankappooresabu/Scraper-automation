810bcbb38b31a6ce25e35112d918e5f0bfe4d08a
amankappooresabu <amansabu99@gmail.com>
2025-11-14 00:31:06 +0530

    Commit 2.0

--- bayut/pipelines.py
+++ bayut/pipelines.py
@@ -10,4 +10,24 @@ from itemadapter import ItemAdapter
 
 class BayutPipeline:
     def process_item(self, item, spider):
+      
+        
+        if item.get('amenities'):
+            amenities_list = item['amenities'].split(',')
+            seen = set()
+            cleaned_amenities = []
+            for amenity in amenities_list:
+                amenity = amenity.strip()
+                if amenity and amenity not in seen:
+                    seen.add(amenity)
+                    cleaned_amenities.append(amenity)
+            item['amenities'] = ','.join(cleaned_amenities)
+        
+        if item.get('price'):
+            item['price'] = item['price'].replace(',', '')
+        
+        for key, value in item.items():
+            if isinstance(value, str):
+                item[key] = value.strip()
+        
         return item
--- bayut/spiders/bayut_spider.py
+++ bayut/spiders/bayut_spider.py
@@ -6,15 +6,29 @@ class BayutSpider(scrapy.Spider):
     name = 'bayut_spider'
     allowed_domains = ['bayut.bh']
     start_urls = ['https://www.bayut.bh/en/to-rent/commercial/bahrain/']
+    item_count = 0
+    max_items = 50
 
     def parse(self, response):
         property_urls = response.xpath('//a[@aria-label="Listing link"]/@href').getall()
-        property_urls = list[Any](set[Any](property_urls))  
+        property_urls = list(set(property_urls))
         self.logger.info(f'Found {len(property_urls)} unique property URLs on this page')
         
         for url in property_urls:
+            if self.item_count >= self.max_items:
+               self.logger.info(f'Reached {self.max_items} items limit. Stopping.')
+               return
+            self.item_count += 1
             full_url = response.urljoin(url)
             yield scrapy.Request(full_url, callback=self.parse_property)
+        next_page = response.xpath('//a[@title="Next" and not(@disabled)]/@href').get()
+    
+        if next_page and self.item_count < self.max_items:
+           next_page_url = response.urljoin(next_page)
+           self.logger.info(f'Following next page: {next_page_url}')
+           yield scrapy.Request(next_page_url, callback=self.parse)
+        else:
+           self.logger.info('No more pages or reached item limit')
         
     def parse_property(self, response):
           yield {
@@ -29,5 +43,11 @@ class BayutSpider(scrapy.Spider):
             'currency': response.xpath('//span[@aria-label="Currency"]/text()').get(),
             'price_per': response.xpath('//span[@aria-label="Frequency"]/text()').get(), 
             'furnished': response.xpath('//span[@aria-label="Furnishing"]/text()').get(),
-            'amenities': ','.join(response.xpath('//div[@class="_050edc8e"]/following-sibling::div//span[@class="c0327f5b"]/text()').getall()),
-        }
+            'amenities': ','.join(response.xpath('//span[@class="c0327f5b"]/text()').getall()),
+            'details': response.xpath('//span[@aria-label="Area"]//span/text()').get(),
+            'agent_name': response.xpath('//span[@aria-label="Agent name"]/text()').get(),
+            'number_of_photos': len(response.xpath('//div[@aria-label="Gallery dialog photo grid"]//source[@type="image/webp"]/@srcset').getall()),
+            'breadcrumb': ' > '.join([response.xpath('normalize-space(//div[@aria-label="Breadcrumb"]/div/text())').get()] + response.xpath('//div[@aria-label="Breadcrumb"]//span[@aria-label="Link name"]/text()').getall()),
+            'property_image_urls': response.xpath('//div[@aria-label="Gallery dialog photo grid"]//source[@type="image/webp"]/@srcset').getall(),
+            'property_type': response.xpath('//span[@aria-label="Type"]/text()').get(), 
+            } 
